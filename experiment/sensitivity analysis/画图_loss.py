724c18801304f84d0c660325b19b81fbbd3f3fe3
李瑞昌 <33052678+CaryLi666@users.noreply.github.com>
2024-06-06 19:11:43 +0800

    Update 画图_loss.py

--- experiment/sensitivity analysis/画图_loss.py
+++ experiment/sensitivity analysis/画图_loss.py
@@ -6,15 +6,14 @@ import matplotlib.pyplot as plt
 
 loss_8=np.load('第一次数据保存全/8小时验证集/loss.npy')
 loss_6=np.load('第一次数据保存全/6小时验证集/loss.npy')
-loss_4=np.load('4小时验证集/loss.npy')
+loss_4=np.load('第一次数据保存全/4小时验证集/loss.npy')
 loss_2=np.load('第一次数据保存全/2小时验证集/loss.npy')
 loss_1=np.load('第一次数据保存全/1小时验证集/loss.npy')
 
-loss_length = list(range(len(loss_8)))
 plt.figure()
 # plt.title('Training')
 plt.plot(loss_length,loss_1,'-o',label= '1 hour',markersize=3,color='burlywood')
-plt.plot(loss_length,loss_2,'-v',label= '2 hours',markersize=4,color='plum')
+plt.plot(loss_length,loss_2,'-v',label= '2 hours',markersize=3,color='plum')
 plt.plot(loss_length,loss_4,'-',label= '4 hours',markersize=3,color='red')
 plt.plot(loss_length,loss_6,'--',label= '6 hours',markersize=3,color='cornflowerblue')
 plt.plot(loss_length,loss_8,'-.',label= '8 hours',markersize=3,color='lime')
